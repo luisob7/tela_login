d71571ce4cee1b722554ed0ab1365a388d610166
Luis Obalski <luisobalski2002@gmail.com>
2025-02-21 18:29:20 -0300

    formulario js

---
+++ formulario_login.js
@@ -0,0 +1,67 @@
+var campoLogin = '';
+var campoPassword = '';
+var validade = false;
+var userValido = false;
+var passwordValida = false;
+
+function exportarCampos (login, password) {
+    campoLogin = login;
+    campoPassword = password;
+}
+
+function login(){
+    validade = validarLogin();
+    if (validade === true) {
+        limparCampos();
+        mostrarAlerta("Login realizado com sucesso!");
+    } else {
+        console.log(userValido);
+        console.log(passwordValida);
+        reconhecerErro(userValido, passwordValida);
+    }
+}
+
+function validarLogin(){
+    userValido = validarUsuario(campoLogin);
+    passwordValida = validarSenha(campoPassword);
+    if (userValido === true && passwordValida === true){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validarUsuario (login) {
+    if (login != undefined && login.trim() !== ''){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validarSenha (password) {
+    if (password != undefined && password.trim() !== '' && !password.includes(' ')){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function mostrarAlerta(alerta){
+    alert(alerta);
+}
+
+function reconhecerErro(user, password) {
+    if (user === true) {
+        mostrarAlerta("Por favor, informe sua senha.");
+    } else if (password === true) {
+        mostrarAlerta("Por favor, informe seu usuário.");
+    } else {
+        mostrarAlerta("Por favor, informe seu usuário e sua senha.")
+    }
+}
+
+function limparCampos() {
+    document.getElementById('usuario').value = '';
+    document.getElementById('senha').value = '';
+}
